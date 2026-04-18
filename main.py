346fb13fd2d5e8982d8887ac4f70739a817385b2
Bubzone <Dizumesu@gmail.com>
2026-04-18 15:46:48 +0200

    listapps with gui made, next on list: connect mai and listApps, make so listApps sends info to a file, and reads from it so that itll remember choices

--- main.py
+++ main.py
@@ -18,30 +18,14 @@ def get_active_process():
         name = None
     return name, title
 
-def add_to_group(name):
-        wybor_grupy = ctypes.windll.user32.MessageBoxW(None, "Czy dodać do grupy produktywnych? (kliknięcie NIE doda do grupy drugiej)", "Wybór grupy", win32con.MB_YESNO)
-        if wybor_grupy == win32con.IDYES:
-            A.append(name)
-        elif wybor_grupy == win32con.IDNO:
-            B.append(name)
-
-def program_start():
-    p = Path(Path.home() / "AppData/Roaming/Microsoft/Windows/Start Menu/Programs")
-    for f in p.rglob("*.lnk"):
-        print(f)
-
 def monitor(poll_interval=1.0, min_session=1.0):
     current = None
     start = time.monotonic()
     totals = {}
-    program_start()
     try:
         while True:
             name, title = get_active_process()
             now = time.monotonic()
-            
-            if name in grupy:
-                add_to_group(name)
 
             if name != current:
                 elapsed = now - start
